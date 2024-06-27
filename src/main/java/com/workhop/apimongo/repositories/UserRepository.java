0b45e58a24eff3dc608906f1bb50f376ba8266bd
Seu <helielsouza01234@gmail.com>
2024-06-26 23:44:34 -0300

    User service/repository created and MongoDB connected succesfully

---
+++ src/main/java/com/workhop/apimongo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.workhop.apimongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.workhop.apimongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {}
