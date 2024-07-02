99051ff074bf4b5c9d54a14e0abc297fa5689b33
Seu <helielsouza01234@gmail.com>
2024-07-01 22:28:07 -0300

    Adding comments to posts

--- src/main/java/com/workhop/apimongo/config/Instantiation.java
+++ src/main/java/com/workhop/apimongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.workhop.apimongo.domain.Post;
 import com.workhop.apimongo.domain.User;
 import com.workhop.apimongo.dtos.AuthorDTO;
+import com.workhop.apimongo.dtos.CommentDTO;
 import com.workhop.apimongo.repositories.PostRepository;
 import com.workhop.apimongo.repositories.UserRepository;
 
@@ -41,6 +42,15 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Bora", "Bora dar uma saida hahaha", new AuthorDTO(kvara));
 		Post post2 = new Post(null, sdf.parse("22/03/2018"), "Tá dificil...", "Será que meu time ganha hoje? ... ", new AuthorDTO(johnes));
 		Post post3 = new Post(null, sdf.parse("23/03/2018"), "Bom domingo!", "Qual a boa de hoje ein?", new AuthorDTO(johnes));
+		
+		CommentDTO c1 = new CommentDTO("Oloco nem me chama :(" , sdf.parse("21/03/2024"), new AuthorDTO(suzanna));
+		CommentDTO c2 = new CommentDTO("Boooora" , sdf.parse("21/03/2024"), new AuthorDTO(johnes));
+		CommentDTO c3 = new CommentDTO("Se vai jogar contra o meu, com certeza não hahaha" , sdf.parse("22/03/2024"), new AuthorDTO(kvara));
+		CommentDTO c4 = new CommentDTO("Se achar me avise :)" , sdf.parse("23/03/2024"), new AuthorDTO(suzanna));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		post3.getComments().add(c4);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 		
